cb8cb81c395784678ea29f2c0fc1a7146220412c
michiel <michiel@synetic.nl>
2019-04-01 10:04:03 +0200

    SYNETICSIS-1582 Als developer wil ik weten of de data in de collection nog geldig is en de situatie niet veranderd is zodat ik eventueel de resultaten zonder server-load kan teruggeven

--- test/unit/Resource/ParameterBag.test.js
+++ test/unit/Resource/ParameterBag.test.js
@@ -1,17 +1,55 @@
 import ParameterBag from '../../../src/Resource/ParameterBag';
 
 describe('The parameter bag', () => {
-  const parameterBag = new ParameterBag();
+  test('that it is initialized with default empty values', () => {
+    const parameterBag = new ParameterBag();
 
-  it('should be initialized with default empty values', () => {
     expect(parameterBag.filters).toEqual([]);
     expect(parameterBag.paging).toEqual({});
     expect(parameterBag.sorting).toEqual([]);
     expect(parameterBag.params).toEqual({});
   });
 
-  it('should contain proper setters and getters for filters, paging, sorting and params', () => {
-    let params = {
+  test('that two instances do not share data', () => {
+    const parameterBag = new ParameterBag();
+    const parameterBag2 = new ParameterBag();
+
+    parameterBag.setFilters([{
+      field: 'f1',
+      value: 'v1',
+    }]);
+
+    expect(parameterBag.filters).not.toEqual(parameterBag2.filters);
+  });
+
+  test('that the inner variables are not writable', () => {
+    const parameterBag = new ParameterBag();
+
+    expect(() => {
+      parameterBag.parameters = {};
+    }).toThrow(TypeError);
+
+    expect(() => {
+      parameterBag.filters = [];
+    }).toThrow(TypeError);
+
+    expect(() => {
+      parameterBag.sorting = [];
+    }).toThrow(TypeError);
+
+    expect(() => {
+      parameterBag.paging = {};
+    }).toThrow(TypeError);
+
+    expect(() => {
+      parameterBag.params = [];
+    }).toThrow(TypeError);
+  });
+
+  test('that it contains setters and getters for filters, paging, sorting and params', () => {
+    const parameterBag = new ParameterBag();
+
+    const params = {
       key1: 'value1',
       key2: 'value2',
     };
@@ -53,5 +91,49 @@ describe('The parameter bag', () => {
     params.key3 = 'value3';
     parameterBag.addParam('key3', 'value3');
     expect(parameterBag.params).toBe(params);
+  });
+
+  test('that a stateId is set after setting any parameter', () => {
+    const parameterBagSetFilters = new ParameterBag();
+    expect(parameterBagSetFilters.stateId).toBe(0);
+    parameterBagSetFilters.setFilters([]);
+    expect(parameterBagSetFilters.stateId).not.toBe(0);
+
+    const parameterBagAddFilter = new ParameterBag();
+    expect(parameterBagAddFilter.stateId).toBe(0);
+    parameterBagAddFilter.addFilter({ field: 'test', value: null });
+    expect(parameterBagAddFilter.stateId).not.toBe(0);
+
+    const parameterBagSetPaging = new ParameterBag();
+    expect(parameterBagSetPaging.stateId).toBe(0);
+    parameterBagSetPaging.setPaging({ offset: 0, limit: 10 });
+    expect(parameterBagSetPaging.stateId).not.toBe(0);
+
+    const parameterBagSetSorting = new ParameterBag();
+    expect(parameterBagSetSorting.stateId).toBe(0);
+    parameterBagSetSorting.setSorting([]);
+    expect(parameterBagSetSorting.stateId).not.toBe(0);
+
+    const parameterBagSetParams = new ParameterBag();
+    expect(parameterBagSetParams.stateId).toBe(0);
+    parameterBagSetParams.setParams({});
+    expect(parameterBagSetParams.stateId).not.toBe(0);
+
+    const parameterBagAddParam = new ParameterBag();
+    expect(parameterBagAddParam.stateId).toBe(0);
+    parameterBagAddParam.addParam('key', 'value');
+    expect(parameterBagAddParam.stateId).not.toBe(0);
+  });
+
+  test('that a stateId is changes after multiple calls', () => {
+    const parameterBag = new ParameterBag();
+    expect(parameterBag.stateId).toBe(0);
+
+    parameterBag.setFilters([]);
+    expect(parameterBag.stateId).not.toBe(0);
+
+    const currentStateId = parameterBag.stateId;
+    parameterBag.setFilters([]);
+    expect(parameterBag.stateId).not.toBe(currentStateId);
   });
 });
