c3496997f2bdf32abf15e333c79e8605a42f7331
Jur de Vries <info@jurdevries.nl>
2019-04-30 11:17:16 +0200

    Fixed normalize paging and return promise from collection.load.

--- src/Integrations/Vue/Collection/createVuexCollectionFromState.js
+++ src/Integrations/Vue/Collection/createVuexCollectionFromState.js
@@ -4,18 +4,17 @@ export default function createVuexCollectionFromState(name, state, repository, s
   const collection = Collection.fromState(name, state, repository);
 
   const { load } = collection;
-  collection.load = () => {
-    load().then(() => {
-      store.commit(`hyral_${repository.resourceType}/SET_COLLECTION`, {
-        name: collection.name,
-        state: collection.state,
-      });
-
-      collection.items.forEach((resource) => {
-        store.commit(`hyral_${repository.resourceType}/SET_RESOURCE`, resource);
-      });
+  collection.load = () => load().then(() => {
+    store.commit(`hyral_${repository.resourceType}/SET_COLLECTION`, {
+      name: collection.name,
+      state: collection.state,
     });
-  };
+
+    collection.items.forEach((resource) => {
+      store.commit(`hyral_${repository.resourceType}/SET_RESOURCE`, resource);
+    });
+    return collection;
+  });
 
   return collection;
 }
