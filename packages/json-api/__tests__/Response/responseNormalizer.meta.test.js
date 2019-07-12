57e09d897ccdc81d6f4e77cc1b9619b63de9d691
michiel <michiel@synetic.nl>
2019-07-12 13:19:26 +0200

    (feat) Include meta information on resource level if available #73 (Resource relationship support)

--- packages/json-api/__tests__/Response/responseNormalizer.meta.test.js
+++ packages/json-api/__tests__/Response/responseNormalizer.meta.test.js
@@ -5,10 +5,15 @@ import jsonSingleResponseFixture from '../fixtures/JsonApi/Fetch/fetchJsonSingle
 describe('Resource meta for the responseNormalizer', () => {
   Resource.decorators = [];
 
-  test('that the responseNormalizer returns a single response', () => {
+  test('that the responseNormalizer returns meta information', () => {
     const result = responseNormalizer(jsonSingleResponseFixture);
 
     expect(result.data.meta).toHaveProperty('meta-property');
     expect(result.data.meta['meta-property']).toEqual('test-value');
+
+    expect(result.data.data.images[0].meta).toHaveProperty('meta-relationships-property');
+    expect(result.data.data.images[0].meta).toHaveProperty('image-meta-property');
+    expect(result.data.data.images[0].meta['meta-relationships-property']).toEqual('test-relationships-value');
+    expect(result.data.data.images[0].meta['image-meta-property']).toEqual('image-test-value');
   });
 });
