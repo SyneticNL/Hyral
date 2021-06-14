1cc33f578b0c977ce15b742d6fe1c25d80278c20
Marco van Harten <marcovanharten89@gmail.com>
2021-06-14 14:06:13 +0200

    test(core): update collection tests

--- packages/core/__tests__/Resource/Collection.spec.ts
+++ packages/core/__tests__/Resource/Collection.spec.ts
@@ -2,10 +2,12 @@ import Collection from '../../src/Resource/Collection';
 import Resource from '../../src/Resource/Resource';
 
 describe('Collection tests', () => {
+  const type = 'product';
+  const name = type;
   const mockResponse = {
     data: [
-      new Resource<any>('1', 'products', { title: 'Product 1' }),
-      new Resource<any>('2', 'products', { title: 'Product 2' }),
+      new Resource<any>('1', type, { title: 'Product 1' }),
+      new Resource<any>('2', type, { title: 'Product 2' }),
     ],
   };
 
@@ -14,7 +16,7 @@ describe('Collection tests', () => {
   };
 
   test('that the collection is initialized correctly', () => {
-    const collection = new Collection('product', productRepository as any);
+    const collection = new Collection(name, type, productRepository as any);
 
     expect(collection.name).toEqual('product');
     expect(collection.repository).toBe(productRepository);
@@ -25,7 +27,7 @@ describe('Collection tests', () => {
 
   test('that the collectionn can be initialized with a parameterbag', () => {
     const parameterbag = {};
-    const collection = new Collection('product', productRepository as any, parameterbag as any);
+    const collection = new Collection(name, type, productRepository as any, parameterbag as any);
 
     expect(collection.name).toEqual('product');
     expect(collection.parameterBag).toEqual({});
@@ -36,12 +38,21 @@ describe('Collection tests', () => {
   });
 
   test('that the collection correctly uses the repository to find items', async () => {
-    const collection = new Collection('product', productRepository as any);
+    const collection = new Collection(name, type, productRepository as any);
 
     await collection.load();
     expect(productRepository.find.mock.calls).toHaveLength(1);
     expect(collection.length).toEqual(2);
     expect(collection.items).toHaveLength(2);
     expect(collection.data).toHaveLength(2);
+  });
+
+  test('that the collection load throws an error without any repository', () => {
+    const collection = new Collection(name, type);
+
+    expect.assertions(1);
+    collection.load().catch((err: Error) => {
+      expect(err.message).toEqual('No repository was present in collection product');
+    });
   });
 });
